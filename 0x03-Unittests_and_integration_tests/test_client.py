b4e4460ddc6de9c61642562a4cb094359c3daa59
tommydebisi <tommydebisi@gmail.com>
2023-01-29 20:44:28 +0100

    :seedling: used property mock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
     tests for client.py
 """
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized, param
 
 GithubOrgClient = __import__('client').GithubOrgClient
@@ -24,3 +24,16 @@ class TestGithubOrgClient(unittest.TestCase):
         # call the org method, by calling it get_json gets called
         test_git.org()
         mock_get_json.assert_called_once()
+
+    def test_public_repos_url(self):
+        """ tests the public repo output """
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {'repos_url': "legoo"}
+            git_tes = GithubOrgClient('leggo')
+
+            self.assertEqual(git_tes._public_repos_url, 'legoo')
+
+
+if __name__ == "__main__":
+    unittest.main()
