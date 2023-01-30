0f0d6bc7f889a897d607d1c49720ee2e8f0a8623
tommydebisi <tommydebisi@gmail.com>
2023-01-30 02:11:37 +0100

    :seedling: tested for list of repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,6 +34,20 @@ class TestGithubOrgClient(unittest.TestCase):
 
             self.assertEqual(git_tes._public_repos_url, 'legoo')
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """ more tests on public repos """
+        dic_list = [{"repos_url": "on code"}, {'name': 'on code'}]
+        mock_get_json.return_value = dic_list
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as mock_pub_repo:
+            mock_pub_repo.return_value = "on c"
+            tes_git = GithubOrgClient('doom')
+
+            self.assertEqual(tes_git.repos_payload, dic_list)
+            mock_pub_repo.assert_called_once()
+            mock_get_json.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
